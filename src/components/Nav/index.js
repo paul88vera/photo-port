62db60964e733c2b9debd28e192c5f2b61aaf7b8
paul88vera <paul88vera@gmail.com>
2022-03-28 19:47:18 -0500

    Nav tests passes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 const categories = [
- {
-   name: "commercial",
-   description:
-     "Photos of grocery stores, food trucks, and other commercial projects",
- },
- { name: "portraits", description: "Portraits of people in my life" },
- { name: "food", description: "Delicious delicacies" },
- {
-   name: "landscape",
-   description: "Fields, farmhouses, waterfalls, and the beauty of nature",
- },
+  {
+    name: "commercial",
+    description:
+      "Photos of grocery stores, food trucks, and other commercial projects",
+  },
+  { name: "portraits", description: "Portraits of people in my life" },
+  { name: "food", description: "Delicious delicacies" },
+  {
+    name: "landscape",
+    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+  },
 ];
 
 const Nav = () => {
   return (
     <header>
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
             📸
           </span>
@@ -28,20 +28,19 @@ const Nav = () => {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About me</a>
+            <a data-testid="about" href="#about">
+              About me
+            </a>
           </li>
           <li>
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-           <li
-           className="mx-1"
-           key={category.name}
-         >
-           <span onClick={() => categorySelected(category.name)}>
-             {category.name}
-           </span>
-         </li>
+            <li className="mx-1" key={category.name}>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span>
+            </li>
           ))}
         </ul>
       </nav>
@@ -50,7 +49,7 @@ const Nav = () => {
 };
 
 function categorySelected(name) {
- console.log(`${name} clicked`)
+  console.log(`${name} clicked`);
 }
 
 export default Nav;
